5e1b0f744876859ba64955a0e34931eb60e8e4e3
Naoki Komiya <edsa9654@gmail.com>
2019-05-24 01:08:38 +0900

    tail test

--- tail_command_by_python/main.py
+++ tail_command_by_python/main.py
@@ -1,6 +1,26 @@
 # _*_ coding: utf-8 _*_
 import sys
+import os.path
 
 
-def tail_main(argv=sys.argv[1:]):
-    print('What a wonder!!')
+def tail_main(argv):
+    if __name__ == "__main__":
+        rFile = sys.argv[1]
+        wCnt = 0
+        fileSize = 0
+
+        while 1:
+            if fileSize != os.path.getsize(rFile):
+                f = open(rFile, 'r')
+
+                idx = 0
+                for row in f:
+                    if idx <= wCnt:
+                        pass
+                    else:
+                        print(row)
+                        wcnt += 1
+                    idx += 1
+
+                f.close()
+                fileSize = os.path.getsize(rFile)
