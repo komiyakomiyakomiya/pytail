81bb2a8ac0da3fdaa6ca82a76fb0e9fe66a09fe4
Naoki Komiya <edsa9654@gmail.com>
2019-05-24 15:55:50 +0900

    add option

--- venv/lib/python3.7/site-packages/tail_command_by_python/main.py
+++ venv/lib/python3.7/site-packages/tail_command_by_python/main.py
@@ -1,15 +1,16 @@
 # _*_ coding: utf-8 _*_
 import sys
 
-args_list = sys.argv
-number_of_line_from_end = args_list[1]
-file_path = args_list[2]
 
+def tail_main():
 
-def tail_main(path):
-    with open(path, 'r') as file_contents:
-        return file_contents.readlines()
+    args_list = sys.argv
+    number_of_line_from_end = args_list[1]
+    file_path = args_list[2]
 
+    def read_file(path):
+        with open(path, 'r') as file_contents:
+            return file_contents.readlines()
 
-line_of_contents_list = tail_main(file_path)
-print("".join(line_of_contents_list[(-1 * int(number_of_line_from_end)):]))
+    line_of_contents_list = read_file(file_path)
+    print("".join(line_of_contents_list[(-1 * int(number_of_line_from_end)):]))
