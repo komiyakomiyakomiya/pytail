f0dfb84e8f94c4de89269c094516e57b970e9b5f
Naoki Komiya <edsa9654@gmail.com>
2019-05-24 17:48:34 +0900

    test

--- tail_command_by_python/main.py
+++ tail_command_by_python/main.py
@@ -7,13 +7,10 @@ import argparse
 def tail_main():
     parser = argparse.ArgumentParser(description='argparse sample.')
 
-    # bool オプション
-    parser.add_argument('-e', '--error', action='store_true',
-                        default=False, help='show error (default: show no error)')
     # 数値 オプション
     parser.add_argument('-n', '--number', type=int, help='data number')
     # 文字列オプション
-    parser.add_argument('-f', '--file', type=str, help='data name')
+    parser.add_argument('-f', '--file_path', type=str, help='data file_path')
 
     args = parser.parse_args()
 
@@ -21,11 +18,14 @@ def tail_main():
     # print(args.file)
 
     number_of_line_from_end = args.number
-    file_path = args.file
+    file_path = args.file_path
 
     def read_file(path):
         with open(path, 'r') as file_contents:
             return file_contents.readlines()
 
     line_of_contents_list = read_file(file_path)
-    print("".join(line_of_contents_list[(-1 * int(number_of_line_from_end)):]))
+    result_output = ''.join(
+        line_of_contents_list[-1 * int(number_of_line_from_end):])
+
+    print(result_output)
