7fba1b8d3c301009e1d8b000bb98a79393034700
Naoki Komiya <edsa9654@gmail.com>
2019-05-24 16:12:03 +0900

    test

--- venv/lib/python3.7/site-packages/tail_command_by_python/main.py
+++ venv/lib/python3.7/site-packages/tail_command_by_python/main.py
@@ -1,12 +1,29 @@
-# _*_ coding: utf-8 _*_
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-:q
+
 import sys
+import argparse
+
+parser = argparse.ArgumentParser(description='argparse sample.')
+
+# bool オプション
+parser.add_argument('-e', '--error', action='store_true',
+                    default=False, help='show error (default: show no error)')
+# 数値 オプション
+parser.add_argument('-d', '--data', type=int, help='data number')
+# 文字列オプション
+parser.add_argument('-s', '--str', type=str, help='data name')
+
+args = parser.parse_args()
+
+print(args.data)
+print(args.str)
 
 
 def tail_main():
 
-    args_list = sys.argv
-    number_of_line_from_end = args_list[1]
-    file_path = args_list[2]
+    number_of_line_from_end = args.data
+    file_path = args.str
 
     def read_file(path):
         with open(path, 'r') as file_contents:
