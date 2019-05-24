afdae71ceaa9cf0b053602905b382691a825a540
Naoki Komiya <edsa9654@gmail.com>
2019-05-24 18:44:28 +0900

    test

--- tail_command_by_python/main.py
+++ tail_command_by_python/main.py
@@ -12,20 +12,21 @@ def tail_main():
     # 文字列オプション
     parser.add_argument('-f', '--file_path', type=str, help='data file_path')
     # bool オプション
-    parser.add_argument('-r', '--reverse', type=str, default=False,
-                        help='Use tail_command => True, Use head_command => False')
+    # parser.add_argument('-r', '--reverse', type=str, default=False,
+    # help='Use tail_command => True, Use head_command => False')
 
     args = parser.parse_args()
 
     print(args.number)
     print(args.file)
-    print(args.reverse)
+    # print(args.reverse)
 
     number_of_line_from_end = args.number
     file_path = args.file_path
     is_reverse = args.reverse.title
 
-    reverse_switch = -1 if is_reverse else 1
+    # reverse_switch = -1 if is_reverse else 1
+    reverse_switch = -1
 
     def read_file(path):
         with open(path, 'r') as file_contents:
--- venv/lib/python3.7/site-packages/tail_command_by_python/main.py
+++ venv/lib/python3.7/site-packages/tail_command_by_python/main.py
@@ -7,18 +7,25 @@ import argparse
 def tail_main():
     parser = argparse.ArgumentParser(description='argparse sample.')
 
-    # 数値 オプション
+    # 数値オプション
     parser.add_argument('-n', '--number', type=int, help='data number')
     # 文字列オプション
     parser.add_argument('-f', '--file_path', type=str, help='data file_path')
+    # bool オプション
+    parser.add_argument('-r', '--reverse', type=str, default=False,
+                        help='Use tail_command => True, Use head_command => False')
 
     args = parser.parse_args()
 
-    # print(args.number)
-    # print(args.file)
+    print(args.number)
+    print(args.file)
+    print(args.reverse)
 
     number_of_line_from_end = args.number
     file_path = args.file_path
+    is_reverse = args.reverse.title
+
+    reverse_switch = -1 if is_reverse else 1
 
     def read_file(path):
         with open(path, 'r') as file_contents:
@@ -26,6 +33,6 @@ def tail_main():
 
     line_of_contents_list = read_file(file_path)
     result_output = ''.join(
-        line_of_contents_list[-1 * int(number_of_line_from_end):])
+        line_of_contents_list[reverse_switch * int(number_of_line_from_end):])
 
     print(result_output)
