b8ff2d97fdd65aa6728f25d178473f4df46185c2
Naoki Komiya <edsa9654@gmail.com>
2019-05-24 16:14:25 +0900

    test

--- venv/lib/python3.7/site-packages/tail_command_by_python/main.py
+++ venv/lib/python3.7/site-packages/tail_command_by_python/main.py
@@ -4,26 +4,25 @@
 import sys
 import argparse
 
-parser = argparse.ArgumentParser(description='argparse sample.')
-
-# bool オプション
-parser.add_argument('-e', '--error', action='store_true',
-                    default=False, help='show error (default: show no error)')
-# 数値 オプション
-parser.add_argument('-d', '--data', type=int, help='data number')
-# 文字列オプション
-parser.add_argument('-s', '--str', type=str, help='data name')
-
-args = parser.parse_args()
-
-print(args.data)
-print(args.str)
-
 
 def tail_main():
+    parser = argparse.ArgumentParser(description='argparse sample.')
 
-    number_of_line_from_end = args.data
-    file_path = args.str
+    # bool オプション
+    parser.add_argument('-e', '--error', action='store_true',
+                        default=False, help='show error (default: show no error)')
+    # 数値 オプション
+    parser.add_argument('-n', '--number', type=int, help='data number')
+    # 文字列オプション
+    parser.add_argument('-f', '--file', type=str, help='data name')
+
+    args = parser.parse_args()
+
+    # print(args.number)
+    # print(args.file)
+
+    number_of_line_from_end = args.number
+    file_path = args.file
 
     def read_file(path):
         with open(path, 'r') as file_contents:
